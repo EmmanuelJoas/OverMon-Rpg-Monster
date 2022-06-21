083e4d2b2d78fd7f0382758b0b44e1cb7549f374
EmmanuelJoas <107039075+EmmanuelJoas@users.noreply.github.com>
2022-06-21 15:37:04 +0000

    Optimization of camera movements and addition of comments in the script

--- Assets/Scripts/Engine/Elements Game/CameraFolow.cs
+++ Assets/Scripts/Engine/Elements Game/CameraFolow.cs
@@ -1,14 +1,33 @@
-﻿
+﻿//Name space to use the unity base function 
 using UnityEngine;
 
+/// <summary>
+/// Class use to the mouvement of the camera 
+/// </summary>
 public class CameraFolow : MonoBehaviour
 {
+    #region Variables 
+
+    /// <summary>
+    /// Reference to the player 
+    /// </summary>
     [SerializeField]
     private GameObject player;
-    void Update()
+
+    #endregion
+
+
+    #region Unity function 
+
+    /// <summary>
+    /// Reference to the function who  called after all Update functions have been called
+    /// </summary>
+    void LateUpdate()
     {
         //we move the camera according to the values of our variables 
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y,-10);
 
     }
+
+    #endregion
 }
