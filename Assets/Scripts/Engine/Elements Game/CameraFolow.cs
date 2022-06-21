a8f54e17b39a330f7addb0ba9bcd6c1b33d8eb6c
EmmanuelJoas <107039075+EmmanuelJoas@users.noreply.github.com>
2022-06-21 11:46:58 +0000

    Add actife of the project

---
+++ Assets/Scripts/Engine/Elements Game/CameraFolow.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class CameraFolow : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    void Update()
+    {
+        //we move the camera according to the values of our variables 
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y,-10);
+
+    }
+}
---
+++ Assets/Scripts/Engine/Player/PlayerMovements.cs
@@ -0,0 +1,144 @@
+﻿//Name space to use the collection systeme function 
+using System.Collections;
+
+//use the systeme collection generique function 
+using System.Collections.Generic;
+
+//Name space to use the unity base function 
+using UnityEngine;
+
+// the class who reference the mouvement of the player  
+public class PlayerMovements : MonoBehaviour
+{
+    #region Variables 
+
+    /// <summary>
+    /// Reference to the speed move of the player 
+    /// </summary>
+    [SerializeField]
+    private float speed;
+
+    /// <summary>
+    /// Reference to the sprite renderer of the player 
+    /// </summary>
+    [SerializeField]
+    private SpriteRenderer GraphicsPlayer;
+
+    /// <summary>
+    /// Reference to the animation of the player 
+    /// </summary>
+    [SerializeField]
+    private Animator PlayerAnimation;
+
+    /// <summary>
+    /// Reference to the vecotor move of the player 
+    /// </summary>
+    private Vector2 direction;
+
+    #endregion
+
+    #region Unity function 
+
+    /// <summary>
+    /// Reference to the function who starts at the start af the game 
+    /// </summary>
+    private void Start()
+    {
+        direction = Vector2.zero;
+    }
+
+    /// <summary>
+    /// Reference to the function who throws it at each frame 
+    /// </summary>
+    private void Update()
+    {
+        PlayreMove();
+        GetInput();
+    }
+
+    #endregion
+
+    #region My Private Function 
+
+    /// <summary>
+    /// Reference to the function who called for move the player 
+    /// </summary>
+    private void PlayreMove()
+    {
+
+        transform.Translate(direction*speed*Time.deltaTime);
+        
+    }
+
+    /// <summary>
+    /// Reference to the function who called for get the input of the Keybord 
+    /// </summary>
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction += Vector2.left;
+
+            GraphicsPlayer.flipX = false;
+           
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += Vector2.right;
+
+            GraphicsPlayer.flipX = true;
+        }
+        
+        AnimationPlayerRun(Mathf.Abs(direction.x));
+
+        AnimationPlayerDown(direction.y);
+
+        AnimationPlayerUp(direction.y);
+
+        Debug.Log(direction);
+
+    }
+
+    /// <summary>
+    /// Reference to the function who called for start the animation move "run" of the player 
+    /// </summary>
+    /// <param name="_direction"></param>
+    void AnimationPlayerRun(float _direction)
+    {
+        PlayerAnimation.SetFloat("IsRun",_direction);
+    }
+
+    /// <summary>
+    /// Reference to the function who called for start the animation move "Down" of the player 
+    /// </summary>
+    /// <param name="_direction"></param>
+    void AnimationPlayerDown(float _direction)
+    {
+        PlayerAnimation.SetFloat("IsDown", _direction);
+    }
+
+    /// <summary>
+    /// Reference to the function who called for start the animation move "Up" of the player 
+    /// </summary>
+    /// <param name="_direction"></param>
+    void AnimationPlayerUp(float _direction)
+    {
+        PlayerAnimation.SetFloat("IsUp", _direction);
+    }
+
+    #endregion
+}
+
